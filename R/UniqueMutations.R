b1e4a71abcd77b5d0a95b89b1db5ae34f39a8592
VHIRHepatiques <mercedes.guerrero@uvic.cat>
2018-09-20 12:03:22 +0200

    Package corrections

--- R/UniqueMutations.R
+++ R/UniqueMutations.R
@@ -1,6 +1,6 @@
 UniqueMutations <- 
 function(hseqs){ 
-    if(class(hseqs)!="DNAStringSet" & class(hseqs)!="AAStringSet") 
+    if(!is(hseqs,"DNAStringSet") & !is(hseqs,"AAStringSet"))
         stop("The input object must be DNAStringSet or AAStringSet \n")
     mut.tbl <- MutsTbl(hseqs)
     return(sum(apply(mut.tbl,1,function(x) sum(x>0))))
