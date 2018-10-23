1b6cde8d2d8f29d26f26fd23b1633cfb4d898eeb
VHIRHepatiques <mercedes.guerrero@uvic.cat>
2018-10-23 16:20:12 +0200

    SegSites correction

--- R/SegSites.R
+++ R/SegSites.R
@@ -4,5 +4,5 @@ function(seqs){
         ##  seqs: an aligment of haplotypes
     if(!is(seqs, "DNAStringSet") & !is(seqs, "AAStringSet"))
         stop("The input object must be a DNAStringSet or AAStringSet\n")
-    return(sum(colSums(FreqMat(seqs)) > 0) > 1 )
+    return(sum(colSums(FreqMat(seqs)> 0)> 1))
 }
